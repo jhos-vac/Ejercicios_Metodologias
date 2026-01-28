687136f6b7c772256c32e580a9bb23b9dbc53b4f
drakitoo <bpvillalva@sudamericano.edu.ec>
2026-01-28 08:48:04 -0500

    gg

--- src/ejercicio1/index.ts
+++ src/ejercicio1/index.ts
@@ -2,12 +2,25 @@ import { listasDePrueba } from "../no_modificar/ejercicio1";
 //utilizar la lista de pruebas importada
 listasDePrueba;
 
-export function productoExceptoActual(nums: number[]): number[] {
+export function productoExceptoActual({ nums }: { nums: number[]; }): number[] {
     const n = nums.length;
+    if (n === 0) {
+        return [];
+    }
     const result = new Array(n).fill(1);
 
-   // Escribir tu código aquí  
-   
+
+    let prefix = 1;
+    for (let i = 0; i < n; i++) {
+        result[i] = prefix;
+        prefix *= nums[i]!;
+    }
+
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i]!;
+    }
     
     return result;
 }
--- src/ejercicio3/index.ts
+++ src/ejercicio3/index.ts
@@ -13,6 +13,4 @@ export function sumaMaximaCincoNumeros(lista: number[]): number {
   return suma;
 }
 
-console.log(
-  sumaMaximaCincoNumeros([7,2,5,4,9,0,13,54,12,10,111,3,19,6,15]) //212
-);
+
