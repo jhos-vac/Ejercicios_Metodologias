6abc95f9bc7998ec35e3f8e050b67217881180c6
Jairo Arevalo <jiarevalo@sudamericano.edu.ec>
2026-01-29 09:07:38 -0500

    Update index.ts

--- src/ejercicio2/index.ts
+++ src/ejercicio2/index.ts
@@ -8,42 +8,30 @@
 
 export function transformarCadena(texto: string): string {
   let resultado = "";
-    // Escribir tu código aquí
-    for(let i =0; i < texto.length; i++){
-      const char = texto[i]!;
-      const lowerChar = char.toLowerCase();
 
-      if(lowerChar === 'm' || lowerChar === 'n'){
-        resultado += "*";
-      }
+  for (let i = 0; i < texto.length; i++) {
+    const caracter = texto[i]!;
+    const letra = caracter.toLowerCase();
 
-      else if (char === 'c' || char === 'e'){
-        resultado += "*";
-      }
-
-      else if (char === 't'){
-        resultado += "*)";
-      }
-      else if (['a', 'i', 'o', 'y'].includes(char)){
-        resultado += "(";
-      }
-      else if (char === 'E'){
-        resultado += "(";
-      }
-      else if (char >= 'A' && char <= 'Z'){
-        if (i %2!== 0){
-          resultado += "(";
-        }
-        else{
-          resultado +=")";
-        }
-      }
-      else if (char >= 'a' && char<= 'z'){
-        resultado+= ")";
+    // Cuando las letras son m y n
+    if (letra === "m" || letra === "n") {
+      resultado += "*";
     }
+    // Cuando son Vocales
+    else if (
+      letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"
+    ) {
+      resultado += "(";
+    }
+    // Cuando son letras del alfabeto 
+    else if (letra >= "a" && letra <= "z") {
+      resultado += ")";
+    }
+    // Cuando no son letras (simbolos, numeros, signos)
     else {
-      resultado += char;
+      resultado += caracter;
     }
   }
+
   return resultado;
 }
