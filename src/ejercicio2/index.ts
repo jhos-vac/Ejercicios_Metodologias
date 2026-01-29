46b9298f33b248d0091ce189a58d7a44c7ba40c7
Jairo Arevalo <jiarevalo@sudamericano.edu.ec>
2026-01-28 19:03:43 -0500

    Update index.ts

--- src/ejercicio2/index.ts
+++ src/ejercicio2/index.ts
@@ -9,7 +9,41 @@
 export function transformarCadena(texto: string): string {
   let resultado = "";
     // Escribir tu código aquí
+    for(let i =0; i < texto.length; i++){
+      const char = texto[i]!;
+      const lowerChar = char.toLowerCase();
 
+      if(lowerChar === 'm' || lowerChar === 'n'){
+        resultado += "*";
+      }
 
+      else if (char === 'c' || char === 'e'){
+        resultado += "*";
+      }
+
+      else if (char === 't'){
+        resultado += "*)";
+      }
+      else if (['a', 'i', 'o', 'y'].includes(char)){
+        resultado += "(";
+      }
+      else if (char === 'E'){
+        resultado += "(";
+      }
+      else if (char >= 'A' && char <= 'Z'){
+        if (i %2!== 0){
+          resultado += "(";
+        }
+        else{
+          resultado +=")";
+        }
+      }
+      else if (char >= 'a' && char<= 'z'){
+        resultado+= ")";
+    }
+    else {
+      resultado += char;
+    }
+  }
   return resultado;
 }
