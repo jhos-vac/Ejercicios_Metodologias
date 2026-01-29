19f6643e58fc70a12bd850f5ab9dc8bef9072ea2
Anthony <afrodriguez@sudamericano.edu.ec>
2026-01-29 00:16:10 -0500

    Comentarios Ejercicio 3

--- src/ejercicio3/index.ts
+++ src/ejercicio3/index.ts
@@ -11,13 +11,13 @@ export function sumaMaximaCincoNumeros(lista: number[]): number {
 
     // Escribir tu código aquí
 
-    lista.sort((a, b) => b - a);
+    lista.sort((a, b) => b - a); //función flecha que compara entre mayor y menor (function(a,b){return b-a;};)
 
-    for (let i = 0; i < 5; i++){
-      const valor = lista[i];
-      if(valor !== undefined)
-      suma += valor;
+    for (let i = 0; i < 5; i++){ //bucle que recorre los primeros 5 puestos del array
+      const valor = lista[i]; 
+      if(valor !== undefined) //verifica que el valor no sea igual a "undefined"
+      suma += valor; //se suma el valor actual  
     }
 
-    return suma;
+    return suma; //se devuelve el valor de la suma más alta
 }
