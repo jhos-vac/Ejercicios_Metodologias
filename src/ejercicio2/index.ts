812d2e980d762746be4e5d8bbdac77ff0ec712b2
Anthony <afrodriguez@sudamericano.edu.ec>
2026-01-29 00:05:22 -0500

    Comentarios Ejercicio 2

--- src/ejercicio2/index.ts
+++ src/ejercicio2/index.ts
@@ -10,19 +10,21 @@ export function transformarCadena(texto: string): string {
   let resultado = "";
     // Escribir tu código aquí
 
-for (let i = 0; i < texto.length; i++) {
-  const c = texto.charAt(i).toLowerCase();
+for (let i = 0; i < texto.length; i++) { //bucle que recorre la cadena de texto caracter por caracter
+  const c = texto.charAt(i).toLowerCase(); //texto puede ser undefines; por eso se usa chartAt que siempre devuelve un string
+                            //toLowerCase convierte los caracteres en minúscula
+                            //se define una variable constante c
 
-  if(c === "m" || c === "n") {
-    resultado += "*";
-  }else if ("aeiou".includes(c)){
-    resultado += "(";
-  }else if (c >= "a" && c <= "z") {
-    resultado += ")";
-  }else{
-    resultado += texto[i];
+  if(c === "m" || c === "n") { //condicional si c es igual a "m" o "n"
+    resultado += "*"; //concatenación: reemplaza por "*"
+  }else if ("aeiou".includes(c)){ //si no, otra condicional: si son vocales
+    resultado += "("; //reemplaza por: "("
+  }else if (c >= "a" && c <= "z") { //si no, otra condicional: letras entre a y z (sin tomar en cuenta vocales, m,n)
+    resultado += ")"; //reemplaza por: ")"
+  }else{ //ultima condicional
+    resultado += texto.charAt(i); //si no es una letra devuelve el caracter sin modificar
   }
 }
 
-return resultado;
+return resultado; //devuelve la cadena de texto concatenada
 }
